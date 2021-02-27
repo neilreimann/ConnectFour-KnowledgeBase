d6810cac1d66ffff57cc7945beeb19a3d418869d
neilreimann <nreimann@sympatico.ca>
2021-02-27 12:12:17 -0500

    Removed the Deprecated Code

--- src/main/java/deprecated/com/home/neil/connectfour/knowledgebase/old/KnowledgeBaseFilePoolMBean.java
+++
@@ -1,9 +0,0 @@
-package deprecated.com.home.neil.connectfour.knowledgebase.old;
-
-public interface KnowledgeBaseFilePoolMBean {
-	public String getCurrentUnReservedKnowledgeBaseFilesSize();
-	public String getCurrentLastAccessedUnReservedKnowledgeBaseFilesSize();
-	public String getCurrentReservedKnowledgeBaseFilesSize();
-	public String getCurrentReservationsSize();
-	public String getCurrentKnowledgeBaseUncompressedFiles();
-}
